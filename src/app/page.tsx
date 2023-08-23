6af7fcaea034ad8aca984907738355cb3598d68e
korobprog <korobprog@gmail.com>
2023-08-23 22:55:58 +1000

    inicial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Body } from '@/components/body/body';
+import { Footer } from '@/components/footer/footer';
+import { Header } from '@/components/header/header';
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+}
